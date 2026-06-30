c82fb72ac29f4505e37acdaf7663fa951b6361a0
Wandering Orbz <wanderingorbz@gmail.com>
2026-06-30 21:58:43 +0100

    Create Version 1.1 visual prototype

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,8 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://wanderingaround.co.uk',
+  image: {
+    layout: 'full-width',
+    responsiveStyles: true,
+  },
 });
